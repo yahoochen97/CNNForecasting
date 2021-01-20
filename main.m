83c4b88b466d3395d96dbe5629f05da2d4612e7a
yahoochen97 <chenyehu@umich.edu>
2021-01-20 14:00:02 -0600

    heuristic 1

--- main.m
+++ main.m
@@ -97,10 +97,10 @@ function myrun(tau,type, ls, os, lik, j, mode, plot)
         
         % 2016 in R&R
         % do not need to load 2018/2020 data
-        continue;
-        CNNdata2018 = readData("data/CNNData2018.csv");
-        CNNdata2018(:, ["candidate_name"]) = [];
-        CNNdata = vertcat(CNNdata, CNNdata2018);
+
+%         CNNdata2018 = readData("data/CNNData2018.csv");
+%         CNNdata2018(:, ["candidate_name"]) = [];
+%         CNNdata = vertcat(CNNdata, CNNdata2018);
     elseif mode==3
         % forecast 2020 races
         CNNdata2020 = readData("data/CNNData2020.csv");
